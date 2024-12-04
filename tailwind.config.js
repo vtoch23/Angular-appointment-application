e1847b0e65be4acb902f91a745059dac4fceed87
Vanya Tochevska <Vanya@Mac.communityfibre.co.uk>
2024-12-04 21:24:41 +0000

    updated file structure, moved files outsife of folder

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

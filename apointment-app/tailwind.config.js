d75daf8171d13ab4ef1ff9a1d459cdcccc10808b
Vanya Tochevska <Vanya@Mac.communityfibre.co.uk>
2024-12-04 20:31:25 +0000

    added tailwind css configuration

---
+++ apointment-app/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  // content: [
+  //   "./src/**/*.{html,ts}",
+  // ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
